f1b7ea2a8893f4898003aa299cf6bb3b9f6870c6
cindy1u0 <luo.xiny@northeastern.edu>
2020-09-19 12:44:52 -0400

    changed design package and added firebase config

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,7 +9,18 @@ import rootSaga from "./data/saga";
 import App from "./App";
 import * as firebase from "firebase";
 
-firebase.initializeApp();
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyB3LUG_O4Ev6xOPaEKPgnyjUjUttar3PmA",
+  authDomain: "vocab-buddy-53eca.firebaseapp.com",
+  databaseURL: "https://vocab-buddy-53eca.firebaseio.com",
+  projectId: "vocab-buddy-53eca",
+  storageBucket: "vocab-buddy-53eca.appspot.com",
+  messagingSenderId: "620084102964",
+  appId: "1:620084102964:web:4ea8f577f47430fb208761",
+};
+
+firebase.initializeApp(firebaseConfig);
 
 const sagaMiddleware = createSagaMiddleware();
 
