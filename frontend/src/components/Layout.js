f71e3846ffba23d05c27c4e67240ca919338037d
cindy1u0 <luo.xiny@northeastern.edu>
2020-09-22 00:09:23 -0400

    nav bar in progress

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -2,15 +2,18 @@ import React from "react";
 import { connect } from "react-redux";
 import styled from "styled-components";
 import { singleRequest } from "../data/actions";
+import { LIGHT_PURPLE } from "../constants/colors";
+import { Button } from "antd";
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  width: 100%;
+`;
 
-const TestingLink = styled.a`
-  text-decoration: underline;
-
-  :hover {
-    cursor: pointer;
-  }
+const NavBarContainer = styled.nav`
+  background-color: ${LIGHT_PURPLE};
+  // height: 50px;
+  position: fixed;
+  width: 100%;
 `;
 
 // An example of using a connector
@@ -18,10 +21,11 @@ const connector = connect((state) => ({}), {
   request: singleRequest.request,
 });
 
-const Layout = ({ request }) => {
+const Layout = () => {
   return (
     <Wrapper>
-      <TestingLink onClick={request}>Welcome to vocab buddy</TestingLink>
+      <Button type="primary">Primary Button</Button>
+      <NavBarContainer></NavBarContainer>
     </Wrapper>
   );
 };
