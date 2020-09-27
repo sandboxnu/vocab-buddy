fdefcb98768ecc99f2bdea1af1e6a6a4301c7e18
Stefan Philip <stefankphilip631@gmail.com>
2020-09-27 14:06:19 -0400

    styled component

--- frontend/src/components/PromptSpeech.js
+++ frontend/src/components/PromptSpeech.js
@@ -1,45 +1,17 @@
 import React from "react";
 import Speech from "react-speech";
+import styled from "styled-components";
 import { SoundOutlined } from "@ant-design/icons";
 
-const PromptSpeech = ({ prompt }) => {
-  const speechStyle = {
-    container: {},
-    text: {},
-    buttons: {},
-    play: {
-      hover: {
-        boxShadow: "0 0 15px 0 rgba(108,92,231,.69)",
-        backgroundColor: "GhostWhite",
-      },
-      button: {
-        width: "100",
-        height: "100",
-        cursor: "pointer",
-        pointerEvents: "none",
-        outline: "none",
-        backgroundColor: "#a29bfe",
-        border: "solid 1px #a29bfe",
-        borderRadius: 6,
-      },
-    },
-    pause: {
-      hover: {},
-      button: {},
-    },
-    stop: {
-      hover: {},
-      button: {},
-    },
-    resume: {
-      hover: {},
-      button: {},
-    },
-  };
+const StyledSpeech = styled(Speech)`
+  :hover {
+    boxshadow: "0 0 15px 0 rgba(108,92,231,.69)";
+  }
+`;
 
+const PromptSpeech = ({ prompt }) => {
   return (
-    <Speech
-      styles={speechStyle}
+    <StyledSpeech
       text={prompt}
       pitch={1.05}
       textAsButton={true}
