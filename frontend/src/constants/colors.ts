a0b2e0a2cbacf89d66234c08672e003dc5597a67
Stefan Philip <stefankphilip631@gmail.com>
2020-10-07 01:38:18 -0400

    constants migration

---
+++ frontend/src/constants/colors.ts
@@ -0,0 +1,13 @@
+// Primary colors
+export const CORAL = "#FE5D5D";
+export const INK = "#615EF7";
+export const INK_HOVER = "#7371FF";
+export const SKY = "#A2C5FB";
+export const SEA_FOAM = "#68F2D2";
+export const LOGIN_BACKGROUND = "#CCCFDE";
+
+// RGBA values
+export const CORAL_RGBA = (a: number) => `rgba(254, 93, 93, ${a})`;
+export const INK_RGBA = (a: number) => `rgba(97, 94, 247, ${a})`;
+export const SKY_RGBA = (a: number) => `rgba(162, 197, 251, ${a})`;
+export const SEA_FOAM_RGBA = (a: number) => `rgba(104, 242, 210, ${a})`;
