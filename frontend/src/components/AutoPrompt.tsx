6b1058a851590167e347ee05ac7637fead227015
Stefan Philip <stefankphilip631@gmail.com>
2020-10-25 19:24:19 -0400

    autoprompt timing updates

--- frontend/src/components/AutoPrompt.tsx
+++ frontend/src/components/AutoPrompt.tsx
@@ -7,20 +7,18 @@ interface SpeechProp {
 
 const AutoPrompt : FunctionComponent<SpeechProp> = ({ prompt }) : ReactElement => {
 
-  function talk() {
+  const talk = () => {
     let x = window.speechSynthesis;
     setTimeout(() => {x.speak(new SpeechSynthesisUtterance(prompt))}, 500);
   }
 
   useEffect(() => {
     talk();
-     const interval = setInterval(talk, 30000);
+     const interval = setInterval(talk, 4000);
      return () => clearInterval(interval);
    })
 
-  return (
-        <PromptSpeech prompt={prompt}/>
-  );
+  return <PromptSpeech prompt={prompt}/>;
 };
 
 export default AutoPrompt;
