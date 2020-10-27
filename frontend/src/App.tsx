65f948c06b1319b9c980c5ddf30bd686876bb923
cindy1u0 <luo.xiny@northeastern.edu>
2020-10-27 00:43:34 -0400

    added comments for later changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,7 +7,6 @@ import Quiz from "./pages/Assessments/Quiz";
 import CreateUser from "./pages/Login/CreateUser";
 import Dashboard from "./pages/Dashboard";
 import Interventions from "./pages/Interventions/Interventions";
-import FirstActivity from "./pages/Interventions/FirstActivity";
 import Login from "./pages/Login/Login";
 
 const App = () : ReactElement => {
@@ -18,7 +17,7 @@ const App = () : ReactElement => {
         <Route exact path="/login" component={Login} />
         <Route path="/sign_up" component={CreateUser} />
         <AuthenticatedRoute path="/dashboard" component={Dashboard} />
-        <Route path="/interventions" component={FirstActivity} />
+        <AuthenticatedRoute path="/interventions" component={Interventions} />
         <AuthenticatedRoute exact path="/assessments/:id" component={Quiz} />
         <AuthenticatedRoute path="/assessments" component={Assessments} />
       </Switch>
