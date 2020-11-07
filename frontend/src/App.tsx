e3fdc32662549bd419fa33f765f19dcd5870d38a
dillonhammer <dillonhammer@gmail.com>
2020-11-07 12:40:53 -0500

    Add Fourth Intervention Activity

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,11 @@
 import React, { ReactElement } from "react";
 import { connect } from "react-redux";
-import { BrowserRouter as Router, Redirect, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
 import AuthenticatedRoute from "./components/AuthenticatedRoute";
 import Assessments from "./pages/Assessments/Assessments";
 import Quiz from "./pages/Assessments/Quiz";
@@ -9,11 +14,11 @@ import Dashboard from "./pages/Dashboard";
 import Interventions from "./pages/Interventions/Interventions";
 import Login from "./pages/Login/Login";
 
-const App = () : ReactElement => {
+const App = (): ReactElement => {
   return (
     <Router>
       <Switch>
-        <Redirect exact from="/" to='/login' />
+        <Redirect exact from="/" to="/login" />
         <Route exact path="/login" component={Login} />
         <Route path="/sign_up" component={CreateUser} />
         <AuthenticatedRoute path="/dashboard" component={Dashboard} />
