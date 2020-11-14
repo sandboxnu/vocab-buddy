c45037c653c4e896275cfd6f2b990259e9993285
dillonhammer <dillonhammer@gmail.com>
2020-11-14 02:18:46 -0500

    Add selected image styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,11 @@
 import React, { ReactElement } from "react";
 import { connect } from "react-redux";
-import { BrowserRouter as Router, Redirect, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
 import AuthenticatedRoute from "./components/AuthenticatedRoute";
 import Assessments from "./pages/Assessments/Assessments";
 import Quiz from "./pages/Assessments/Quiz";
@@ -8,16 +13,17 @@ import CreateUser from "./pages/Login/CreateUser";
 import Dashboard from "./pages/Dashboard";
 import Interventions from "./pages/Interventions/Interventions";
 import Login from "./pages/Login/Login";
+import SecondActivity from "./pages/Interventions/SecondActivity";
 
-const App = () : ReactElement => {
+const App = (): ReactElement => {
   return (
     <Router>
       <Switch>
-        <Redirect exact from="/" to='/login' />
+        <Redirect exact from="/" to="/login" />
         <Route exact path="/login" component={Login} />
         <Route path="/sign_up" component={CreateUser} />
         <AuthenticatedRoute path="/dashboard" component={Dashboard} />
-        <AuthenticatedRoute path="/interventions" component={Interventions} />
+        <AuthenticatedRoute path="/interventions" component={SecondActivity} />
         <AuthenticatedRoute exact path="/assessments/:id" component={Quiz} />
         <AuthenticatedRoute path="/assessments" component={Assessments} />
       </Switch>
--- frontend/src/pages/Interventions/FirstActivity.tsx
+++ frontend/src/pages/Interventions/FirstActivity.tsx
@@ -1,14 +1,14 @@
-import React, { ReactElement }  from "react";
+import React, { ReactElement } from "react";
 import styled from "styled-components";
 import Layout from "../../components/Layout";
 import PromptSpeech from "../../components/PromptSpeech";
-import ReplayButton from '../../components/ReplayButton';
+import ReplayButton from "../../components/ReplayButton";
 import PurpleButton from "../../components/PurpleButton";
 import { SKY } from "../../constants/colors";
 
 interface FirstActivityProps {
-  title: string,
-  imageUrl: string,
+  title: string;
+  imageUrl: string;
 }
 
 const Container = styled.div`
@@ -37,7 +37,7 @@ const WordTitle = styled.p`
   margin-bottom: 10px;
   text-transform: lowercase;
   word-wrap: break-word;
- 
+
   @media (max-width: 600px) {
     font-size: 30px;
   }
@@ -73,29 +73,34 @@ const ButtonContainer = styled.div`
   justify-content: end;
 `;
 
-const FirstActivity  = ({ title, imageUrl }: FirstActivityProps) : ReactElement => {
+const FirstActivity = ({
+  title,
+  imageUrl,
+}: FirstActivityProps): ReactElement => {
   return (
-  <Layout>
-    <Container>
-      <MainContent>
-        <DescriptionText>
-          introduction + definition
-        </DescriptionText>
-        <WordTitle>
-          {/* {title} */}
-          Miniscule
-        </WordTitle>
-        <Prompt>
-          <PromptSpeech prompt="“Look at that miniscule ant! It is really, really, small.”" button={<ReplayButton scale={0.8}/>} />
-        </Prompt>
-        {/* <Image src={imageUrl}/> */}
-        <Image src="https://firebasestorage.googleapis.com/v0/b/vocab-buddy-53eca.appspot.com/o/jSyyDnxzx41VFQNQbbEw%2Fminiscule2.png?alt=media&amp;token=f14c983c-6fff-475d-84ba-07b7b86ea2d5" />
-        <ButtonContainer>
-          <PurpleButton text={"next"} top={20}/>
-        </ButtonContainer>
-      </MainContent>
-    </Container>
-  </Layout>);
-}
+    <Layout>
+      <Container>
+        <MainContent>
+          <DescriptionText>introduction + definition</DescriptionText>
+          <WordTitle>
+            {/* {title} */}
+            Miniscule
+          </WordTitle>
+          <Prompt>
+            <PromptSpeech
+              prompt="“Look at that miniscule ant! It is really, really, small.”"
+              button={<ReplayButton scale={0.8} />}
+            />
+          </Prompt>
+          {/* <Image src={imageUrl}/> */}
+          <Image src="https://firebasestorage.googleapis.com/v0/b/vocab-buddy-53eca.appspot.com/o/jSyyDnxzx41VFQNQbbEw%2Fminiscule2.png?alt=media&amp;token=f14c983c-6fff-475d-84ba-07b7b86ea2d5" />
+          <ButtonContainer>
+            <PurpleButton text={"next"} top={20} />
+          </ButtonContainer>
+        </MainContent>
+      </Container>
+    </Layout>
+  );
+};
 
 export default FirstActivity;
