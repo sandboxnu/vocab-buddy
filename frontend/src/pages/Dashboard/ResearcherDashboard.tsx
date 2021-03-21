1a7fc74b0dc2be627f2eca2ce51e4e4ea5368866
angelinahan <han.ang@northeastern.edu>
2021-03-21 19:51:30 -0400

    added ability to sort student cards and styling improvements to dropdown

--- frontend/src/pages/Dashboard/ResearcherDashboard.tsx
+++ frontend/src/pages/Dashboard/ResearcherDashboard.tsx
@@ -1,8 +1,9 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useState } from "react";
 import styled from "styled-components";
 import { User } from "../../models/types";
 import { Dropdown, Menu } from "antd";
 import rocketSVG from "../../assets/rocket.svg";
+import caret from "../../assets/caret.svg";
 import { CLOUD, SKY } from "../../constants/colors";
 
 const ResearcherDashboardContainer = styled.div`
@@ -29,10 +30,6 @@ const ResearcherTopBar = styled.div`
   flex: 7;
   display: flex;
   justify-content: space-between;
-`;
-
-const StudentFilter = styled(Dropdown)`
-  background: ${CLOUD};
 `;
 
 const StudentCardContainer = styled.div`
@@ -78,6 +75,22 @@ const StudentCardLabel = styled.p`
   }
 `;
 
+const StudentFilter = styled(Dropdown)`
+  background: ${CLOUD};
+  padding: 10px;
+  border-radius: 12px;
+`;
+
+const DropdownLabel = styled.p`
+  font-size: 18px;
+  line-height: 0px;
+`;
+
+const DropdownMenu = styled(Menu)`
+  background: ${CLOUD};
+  border-radius: 12px;
+`;
+
 interface StudentCardParams {
   student: User;
 }
@@ -89,34 +102,87 @@ const StudentCard: FunctionComponent<StudentCardParams> = ({
     <StudentCardContainer>
       <Avatar src={rocketSVG} />
       <StudentCardLabel>{student.name}</StudentCardLabel>
+      <StudentCardLabel>Age: {student.age}</StudentCardLabel>
+      <StudentCardLabel>
+        SessionId: {student.sessionId}
+      </StudentCardLabel>
     </StudentCardContainer>
   );
 };
 
-const menu = (
-  <Menu>
-    <Menu.Item key="0">alphabetical</Menu.Item>
-    <Menu.Item key="1">age</Menu.Item>
-    <Menu.Item key="3">current session</Menu.Item>
-  </Menu>
-);
+const sortByName = (studentA: User, studentB: User): number => {
+  const aName = studentA.name.toLowerCase();
+  const bName = studentB.name.toLowerCase();
+  if (aName > bName) {
+    return 1;
+  } else if (bName > aName) {
+    return -1;
+  }
+  return 0;
+};
+
+const sortByAge = (studentA: User, studentB: User): number => {
+  return studentA.age - studentB.age;
+};
+
+const sortByProgress = (studentA: User, studentB: User): number => {
+  if (
+    studentA.sessionId === undefined ||
+    studentA.sessionId < studentB.sessionId
+  ) {
+    return -1;
+  } else if (
+    studentA.sessionId > studentB.sessionId ||
+    studentB.sessionId === undefined
+  ) {
+    return 1;
+  } else return 0;
+};
 
 interface ResearcherDashboardParams {
   students: User[];
 }
+
 const ResearcherDashboard: FunctionComponent<ResearcherDashboardParams> = ({
   students,
 }) => {
+  const [sortBy, setSortBy] = useState(() => sortByName);
+  const [sortLabel, setSortLabel] = useState("alphabetical");
+
+  const menuOnClick = ({ key }: any) => {
+    if (key === "alphabetical") {
+      setSortBy(() => sortByName);
+    }
+    if (key === "age") {
+      setSortBy(() => sortByAge);
+    }
+    if (key === "current session") {
+      setSortBy(() => sortByProgress);
+    }
+    setSortLabel(key);
+  };
+
+  const menu = (
+    <DropdownMenu onClick={menuOnClick}>
+      <Menu.Item key="alphabetical">alphabetical</Menu.Item>
+      <Menu.Item key="age">age</Menu.Item>
+      <Menu.Item key="current session">current session</Menu.Item>
+    </DropdownMenu>
+  );
+
   return (
     <ResearcherDashboardContainer>
       <ResearcherTopBar>
         <TitleText>students</TitleText>
         <StudentFilter overlay={menu}>
-          <p>sort by: </p>
+          <DropdownLabel>
+            sort by: <strong>{sortLabel}</strong>
+            <img src={caret}></img>
+          </DropdownLabel>
         </StudentFilter>
       </ResearcherTopBar>
       <AllStudentsContainer>
-        {students.map((student) => (
+        {students.sort(sortBy).map((student) => (
           <StudentCard student={student} />
         ))}
       </AllStudentsContainer>
