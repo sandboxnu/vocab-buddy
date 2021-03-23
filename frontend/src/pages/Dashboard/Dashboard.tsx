9f4d3d1cd69832c154f8168d49127dc9c3513a7d
Jack Rosen <jrosen081@gmail.com>
2021-03-22 20:48:55 -0400

    Fix sizing of dashboard so it does not overflow on laptop screens (#76)

--- frontend/src/pages/Dashboard/Dashboard.tsx
+++ frontend/src/pages/Dashboard/Dashboard.tsx
@@ -77,10 +77,10 @@ const SignOutButton = styled.button`
 const DashboardContainer = styled.div`
   display: flex;
   flex-direction: row;
-  justify-content: space-between;
-  min-height: 100%;
-  min-width: 100%;
-  padding-top: 98px;
+  align-items: stretch;
+  width: 100%;
+  padding-top: 70px;
+  min-height: 100vh;
 
   @media (max-width: 900px) {
     flex-direction: column;
@@ -97,7 +97,9 @@ const MenuContainer = styled.div`
   flex: 1;
   border-radius: 0px 12px 0px 0px;
 
-  padding: 64px 64px 64px 64px;
+  padding-top: 3vh;
+  padding-left: 1vh;
+  padding-right: 1vh;
 
   background: ${CLOUD};
 
@@ -126,6 +128,7 @@ const TitleText = styled.p`
   font-weight: 700;
   text-transform: lowercase;
   word-wrap: break-word;
+  margin-top: 1vh;
 
   @media (max-width: 900px) {
     font-size: 30px;
@@ -139,8 +142,8 @@ const MenuTopDiv = styled.div`
 `;
 
 const ProfilePicture = styled.img`
-  height: 183px;
-  width: 183px;
+  height: 9vmax;
+  width: 9vmax;
 
   @media (max-width: 900px) {
     height: 148px;
@@ -154,10 +157,8 @@ const SessionContainer = styled.div`
   align-items: flex-start;
 
   flex: 3;
-  min-height: 100%;
-  padding-left: 64px;
-  padding-top: 64px;
-  padding-bottom: 64px;
+  padding-left: 3vw;
+  padding-top: 3vh;
 
   @media (max-width: 900px) {
     min-width: 100%;
@@ -171,11 +172,11 @@ const SessionCardContainer = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
   min-width: 100%;
-  grid-gap: 32px;
+  gap: 3vmin;
 
   @media (max-width: 900px) {
     grid-template-columns: 1fr 1fr;
-    grid-gap: 24px;
+    gap: 24px;
   }
 `;
 
@@ -184,11 +185,11 @@ interface SessionCompletionProp {
 }
 
 const SessionBox = styled.div`
-  padding: 40px;
+  padding: 2vh 0;
   border-radius: 12px;
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
 
   @media (max-width: 900px) {
@@ -204,6 +205,7 @@ const SessionNumber = styled.p`
   font-size: 1.5vw;
   text-align: center;
   white-space: nowrap;
+  margin-bottom: 0;
 
   @media (max-width: 900px) {
     font-size: 3vw;
@@ -211,8 +213,8 @@ const SessionNumber = styled.p`
 `;
 
 const SessionImage = styled.img`
-  max-width: 100%;
-  margin-bottom: 24px;
+  max-width: 60%;
+  margin-bottom: 2vh;
   @media (max-width: 900px) {
     margin-bottom: 22px;
   }
@@ -221,7 +223,7 @@ const SessionImage = styled.img`
 const NextSessionButton = styled(PurpleButton)`
   min-width: 32%;
   height: 100px;
-  margin-bottom: 64px;
+  margin-bottom: 4vh;
   text-transform: lowercase;
 
   @media (max-width: 900px) {
@@ -231,11 +233,10 @@ const NextSessionButton = styled(PurpleButton)`
 
 const WeekProgressContainer = styled.div`
   flex: 2;
-  min-height: 100%;
 
-  padding-left: 64px;
-  padding-right: 64px;
-  padding-top: 64px;
+  padding-left: 4vw;
+  padding-right: 1vw;
+  padding-top: 3vh;
 
   @media (max-width: 900px) {
     min-width: 100%;
@@ -246,8 +247,9 @@ const WeekProgressContainer = styled.div`
 `;
 
 const ProgressStatsContainer = styled.div`
-  display: flex;
-  flex: 1;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 3vmin;
   flex-wrap: wrap;
   justify-content: space-between;
   min-width: 100%;
@@ -257,34 +259,25 @@ const ProgressBox = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding: 30px 30px 30px 30px;
-
-  flex: 1 0 50%;
-  order: 0;
-  flex-grow: 1;
-
-  max-width: calc(50% - 16px);
-  margin-bottom: 32px;
+  padding: 0.5vh 0.5vw;
 
   background: ${CLOUD};
   border-radius: 12px;
 
   @media (max-width: 900px) {
-    max-width: calc(50% - 12px);
     margin-bottom: 24px;
+    padding: 2vh 0.5vw;
   }
 `;
 
 const ProgressStatNumber = styled.p`
   font-family: "Rubik";
-  font-size: 4vw;
+  font-size: 3vw;
   font-weight: 700;
   text-transform: lowercase;
   color: ${INK};
-  flex: none;
-  order: 0;
-  flex-grow: 0;
-  margin: 0px 0px;
+  flex: 1;
+  margin-bottom: 0;
 
   @media (max-width: 900px) {
     font-size: 6vw;
@@ -300,10 +293,7 @@ const ProgressStatDescription = styled.p`
   word-wrap: break-word;
   text-align: center;
 
-  flex: none;
-  order: 1;
-  flex-grow: 0;
-  margin: 0px 0px;
+  flex: 1;
 
   @media (max-width: 900px) {
     font-size: 3vw;
@@ -314,6 +304,8 @@ const DayLabel = styled.p<DayLabelType>`
   font-size: 1.25vw;
   font-weight: ${({ isToday }) => (isToday ? 700 : 400)};
   line-height: 0px;
+  margin-bottom: 0;
+  margin-top: 3vh;
 
   @media (max-width: 900px) {
     font-size: 3vw;
@@ -327,7 +319,19 @@ const DayLabel = styled.p<DayLabelType>`
 const Star = styled.img`
   height: 51px;
   width: 52px;
-  margin-bottom: 20px;
+
+  @media (max-width: 900px) {
+    height: 35px;
+    width: 36px;
+  }
+`;
+
+const DotContainer = styled.div`
+  height: 51px;
+  width: 52px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
   @media (max-width: 900px) {
     height: 35px;
@@ -337,12 +341,10 @@ const Star = styled.img`
 const Dot = styled.img`
   height: 12px;
   width: 12px;
-  margin: 18px 20px 38px 20px;
 
   @media (max-width: 900px) {
     height: 10px;
     width: 10px;
-    margin: 12px 15px 32px 15px;
   }
 `;
 
@@ -353,8 +355,6 @@ const WeekContainer = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: flex-end;
-  padding: 32px;
-  margin: 32px 0px 64px;
   background: ${CLOUD};
   border-radius: 12px;
   width: 100%;
@@ -369,7 +369,13 @@ const DayContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  width: 50px;
+  flex: 1;
+  padding: 3vh 0;
+  padding-left: 0.5vh;
+  padding-right: 0.5vh;
+  @media (min-width: 901px) {
+    margin: 1vh 0;
+  }
 `;
 
 const ProfileGroup = styled.button`
@@ -487,7 +493,9 @@ const DayOfWeek: FunctionComponent<DayParams> = ({
       {isActive ? (
         <Star src={star}></Star>
       ) : (
-        <Dot src={ellipse}></Dot>
+        <DotContainer>
+          <Dot src={ellipse}></Dot>
+        </DotContainer>
       )}
       <DayLabel isToday={istoday}>{name}</DayLabel>
     </DayContainer>
