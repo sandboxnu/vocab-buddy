5a6ac1c3cfcb7163a136f9010ffb18f3b845606d
Sarah Witt <witt.s@husky.neu.edu>
2021-04-22 10:42:45 -0400

    Add script to delete users

--- frontend/cypress/integration/login.spec.js
+++ frontend/cypress/integration/login.spec.js
@@ -1,6 +1,21 @@
+const serviceAccountJson = Cypress.env("CREDENTIALS");
+// To get credentials for a service account, follow instructions here and then put url to path credentials below:
+// NOTE: env variable must be named CYPRESS_CREDENTIALS and config file must be in the cypress/config/ folder
+// https://cloud.google.com/docs/authentication/getting-started
+
 describe("User can create a student account", () => {
+  let testEmail = "test@example.com";
   it("can sign up", function () {
     cy.visit("/sign_up").then(() => {
+      console.log("here");
+      cy.exec("node ./cypress/utils.js", {
+        env: { userEmail: testEmail, serviceAccount: serviceAccountJson },
+      }).then((result) => {
+        console.log(result.code);
+        console.log(result.stderr);
+        console.log(result.stdout);
+      });
+
       cy.get('[placeholder="name"]')
         .should("be.visible")
         .click()
@@ -10,7 +25,7 @@ describe("User can create a student account", () => {
       cy.get('[placeholder="email address"]')
         .should("be.visible")
         .click()
-        .type("test@example.com");
+        .type(testEmail);
 
       var password = Math.random().toString(36) + "a1!";
       cy.get('[placeholder="password"]')
