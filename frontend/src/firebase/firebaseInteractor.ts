581af20c1acb377005e3b57696bd04efd111edaa
Dillon Scott <dillonscott.2024@gmail.com>
2023-07-30 21:56:03 -0400

    move variables to .env

--- frontend/src/firebase/firebaseInteractor.ts
+++ frontend/src/firebase/firebaseInteractor.ts
@@ -23,7 +23,13 @@ import {
 } from "../models/types";
 
 // Your web app's Firebase configuration
-var firebaseConfig = require("./firebase-config.json");
+var firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  appId: process.env.REACT_APP_APP_ID,
+};
 
 firebase.initializeApp(firebaseConfig);
 
